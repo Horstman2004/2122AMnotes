dd5ce0f6fad43c973b42e2b22837a0d512763ebd
Horstman2004 <adam.horstman@evsck12.com>
2022-01-14 10:03:02 -0600

    CATREV2

--- CSP/Unit1/Notes/Section2/CatchATurtleRev2.py
+++ CSP/Unit1/Notes/Section2/CatchATurtleRev2.py
@@ -1,7 +1,15 @@
 #-----import statements-----
-from operator import truediv
 import turtle as t
 import random as r
+
+#-----Leaderboard-----------
+FILENAME = "leaderboard.txt"
+#read, write or append
+#write - overwrites all data
+#append - will add data to the bottom of the file
+with open(FILENAME, "r") as f:
+    file = f.readlines()    #file is a list of the data
+    f.close()
 
 #-----game configuration----
 wn = t.Screen()
@@ -9,15 +17,14 @@ SCREENW,SCREENH = 300,300
 score=0
 timer=5
 timesUp=False
-countdownstart=True
 counterInterval = 1000
-click=0
 #tuple variable = ("fontstyle",fontsize,"font (B,I,U)")
 fontSetup=("Comic Sans MS", 30, "normal")
 
 #-----initialize turtle-----
 mo = t.Turtle()
 mo.shape("turtle")
+mo.shapesize(2)
 mo.fillcolor("brown")
 mo.speed(0)
 
@@ -33,49 +40,33 @@ counter.penup()
 counter.goto(100,165)
 counter.pendown()
 counter.speed(0)
-
-accuracytxt = t.Turtle()
-accuracytxt.hideturtle()
-accuracytxt.penup()
-accuracytxt.goto(100,130)
-accuracytxt.pendown()
-accuracytxt.speed(0)
 #-----game functions--------
-def countdown2():
+def updateHS():
+    with open(FILENAME,"a") as f:
+        f.write("AAA,10")
+        f.close()
+    
+def countdown():
     global timer, timesUp
     counter.clear()
     if timer<=0:
         counter.write("Times Up!",font=fontSetup)
         timesUp=True
-        mo.hideturtle()
+        updateHS()
     else:
         counter.write(f'Time: {timer}',font=fontSetup)
         timer-=1
-        counter.getscreen().ontimer(countdown2,counterInterval)
+        counter.getscreen().ontimer(countdown,counterInterval)
         print(timer)
 
-def countdown1():
-    global timer, timesUp
-    counter.clear()
-    if timer<=0:
-        counter.write("Times Up!",font=fontSetup)
-        timesUp=True
-        mo.hideturtle
-    else:
-        counter.write(f'Time: {timer}',font=fontSetup)
-        timer-=1
-        counter.getscreen().ontimer(countdown1,counterInterval)
-        print(timer)
-                
 def updateScore():
     global score
     #it will see the var score in other parts of the program
     score+=1
     scorekeeper.clear()      #clear what it already wrote
     #write the score
-    scorekeeper.write(f"Score: {score}",font=fontSetup) 
-    decreaseSize()
-
+    scorekeeper.write(f"Score: {score}",font=fontSetup)
+     
 def changePosition():
     #move the turtle to a random location on the screen
     newX = r.randint(-SCREENW/2,SCREENW/2)
@@ -90,33 +81,20 @@ def changePosition():
     mo.goto(newX,newY)
     mo.showturtle()
     mo.pendown()
-    wn.bgcolor("red")
 
 #no matter what, if it is a mouse click
 #    pass in x and y
 def moClicked(x,y): 
-#x and y are the cuursor's coordinates
+    #x and y are the cuursor's coordinates
+    print(x,y)
+    print("mo was clicked")
     changePosition()
     updateScore()
-    
-def decreaseSize():
-    size=mo.turtlesize()
-    decrease = tuple([2 / num for num in size])
-    mo.turtlesize(*decrease)
-    wn.bgcolor("white") 
-    
-def clickAccuracy(x,y):
-    global score, click
-    click+=1
-    accuracy= round((score/click)*100,0)
-    mo.showturtle
-    mo.clear()
-    accuracytxt.write(f"Accuracy: {accuracy}%", font=fontSetup)
-    
+
+
 #-----events----------------
 #obj.method(command or function name)
-wn.onscreenclick(clickAccuracy)
-wn.ontimer(countdown2,counterInterval) 
-wn.ontimer(countdown1,counterInterval)     #"acts" like the clock widget from MIT
+
+wn.ontimer(countdown,counterInterval)     #"acts" like the clock widget from MIT
 mo.onclick(moClicked)
 wn.mainloop()
