aa9284b192d06f39456a4bf6a7f771cc0fed86fc
Horstman2004 <adam.horstman@evsck12.com>
2022-02-14 10:02:35 -0600

    Lua Pong

---
+++ CSP/Unit1/Notes/Lua/LUAPong/pong4CPU/main.lua
@@ -0,0 +1,169 @@
+--var = import or require the file 'push'
+push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
+
+--A function to start up a window that initializes the game
+function love.load()
+
+    --"seed" the RNG so that calls to random are always random
+    --use the current time, since that will vary on startup every time
+    math.randomseed(os.time())
+    
+    --use nearest neogbor filtering on upscalling and downscalling to prevent blurry images or words
+    love.graphics.setDefaultFilter('nearest','nearest')
+
+    --var = love's graphics module.newfont('file',fontsize)
+    smallFont = love.graphics.newFont('font.ttf',8)
+    scoreFont = love.graphics.newFont('font.ttf',24)
+    --set font for love
+    love.graphics.setFont(smallFont)
+
+    -- Method changes the sscreen to our virtual resolution
+    push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT-50
+
+    ballx = VIRTUAL_WIDTH / 2 - 2
+    bally = VIRTUAL_HEIGHT / 2 - 2
+
+    ball = VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,4,4
+    --determining a random dx and y to move the ball
+    ballDx = math.random(2) == 1 and 100 or -100
+    ballDy = math.random(-50,50)
+
+    --this var will help determine what is going on in the game
+    --in the beginning, menu, main game, high score, etc
+    gameState = 'start'
+end
+
+--this function is ran every frame
+--dt is delta time or change in seconds since the last frame
+function love.update(dt)
+    --player 1 movement
+    if love.keyboard.isDown('w') then
+        --move up by add a negative paddle speed to the y scaled by dt
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        --move up by add a positive paddle speed to the y scaled by dt
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    --player 2 movement
+    if love.keyboard.isDown('e') then
+        --move up by add a negative paddle speed to the y scaled by dt
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('d') then
+        --move up by add a positive paddle speed to the y scaled by dt
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+
+    --ball movement
+    if gameState == 'play' then
+        --scale the velocity by dt so movement is framrate-independent
+        ballx = ballx + ballDx * dt
+        bally = bally + ballDy * dt
+    end
+
+    --Collision
+    if ball == player1 then
+        ballDx = -ballDx * 1.03
+        ballx = player1 + 5
+        if ballDy<0 then
+             ballDy = -math.random(10,150)
+        else
+             ballDy = math.random(10,150)
+        end
+    end
+
+   if ball == player2 then
+        ballDx = -ballDx * 1.03
+        ballx = player2 + 5
+        if ballDy<0 then
+            ballDy = -math.random(10,150)
+        else
+            ballDy = math.random(10,150)
+        end
+    end
+
+    if bally <= 0 then
+        bally = 0
+        ballDy = -ballDy
+    end
+
+    if bally >= VIRTUAL_HEIGHT - 4 then
+        bally = VIRTUAL_HEIGHT - 4
+        ballDy = -ballDy
+    end
+
+    
+
+end
+
+--Check for any keypressing
+function love.keypressed(key)
+    --keys can be accessed by string name
+    if key=='escape' then
+        love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState='play'
+        else
+            gameState='start'
+            ballx = VIRTUAL_WIDTH / 2 - 2
+            bally = VIRTUAL_HEIGHT / 2 - 2
+            --and/or pattern here is Lua's way of accomplishing a ternary operation
+            --very common in other programming languages like C
+            ballDx = math.random(2) == 1 and 100 or -100
+            ballDy = math.random(-50,50)
+        end
+    end
+end
+
+--draw on the screen the letters
+--update the screen with the infromation:  draw anything that is needed
+function love.draw()
+    -- begin rending at a virtual resolution
+    push:apply('start')
+
+    --clear the screen and reset the background color
+    --love.graphics.clear(r,g,b,a)
+    love.graphics.clear(40,45,52,255)
+    --set font for love
+    love.graphics.setFont(smallFont)
+
+    if gameState == 'start' then
+        love.graphics.printf('Hello Start State',0,20,VIRTUAL_WIDTH,'center')
+    else
+        love.graphics.printf('Hello Play State',0,20,VIRTUAL_WIDTH,'center')
+    end
+
+    --love.graphics.printf('Hello Pong!',0,20,VIRTUAL_WIDTH,'center')
+
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score),VIRTUAL_WIDTH/2-50,VIRTUAL_HEIGHT/3)
+    love.graphics.print(tostring(player2Score),VIRTUAL_WIDTH/2+50,VIRTUAL_HEIGHT/3)
+    --Draw the paddles and the ball
+
+    --love.graphics.setting.rectangle('filled or not',x,y,width,height)
+    love.graphics.rectangle('fill',10,player1Y,5,20)
+    --second paddle
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH-10,player2Y-50,5,20)
+    --render ball
+    love.graphics.rectangle('fill',ballx,bally,4,4)
+    --end virtual resolution
+    push:apply('end')
+end
--- CSP/Unit1/Notes/Lua/LUAPong/pong7/main.lua
+++ CSP/Unit1/Notes/Lua/LUAPong/pong7/main.lua
@@ -27,12 +27,18 @@ function love.load()
      smallFont = love.graphics.newFont('font.ttf',8)
      scoreFont = love.graphics.newFont('font.ttf',32)
 
-
+     --Sound Table
+     sounds = {
+          --key = love.audio.newSource('filepath','static')
+          ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav','static'),
+          ['score'] = love.audio.newSource('sounds/score.wav','static'),
+          ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav','static')
+     }
 
      --this method changes the screen to our virtual resolution
      push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
           fullscreen=false,
-          resizable=false,
+          resizable=true,
           vsync=true
      })
 
@@ -49,10 +55,17 @@ function love.load()
 
 end
 
+
+
+function love.resize(w,h)
+     push:resize(w,h)
+end
+
+
 --this f(x) is ran every frame, pass in dt. 
 --dt is delta time or change in seconds since the last frame
 function love.update(dt)
---Ball Collision
+     --Ball Collision
      if ball:collides(player1) then
           ball.dx = -ball.dx * 1.03
           ball.x = player1.x + 5
@@ -61,6 +74,7 @@ function love.update(dt)
           else
                ball.dy = math.random(10, 150)
           end
+          sounds['paddle_hit']:play()
      end
 
      if ball:collides(player2) then
@@ -71,22 +85,26 @@ function love.update(dt)
           else
                ball.dy = math.random(10, 150)
           end
+          sounds['paddle_hit']:play()
      end
 
      if ball.y <= 0 then
           ball.y = 0
           ball.dy = -ball.dy
+          sounds['wall_hit']:play()
      end
 
      if ball.y >= VIRTUAL_HEIGHT - 4 then
           ball.y = VIRTUAL_HEIGHT - 4
           ball.dy = -ball.dy
+          sounds['wall_hit']:play()
      end
 
 
--- if we reach the left or right edge of the screen, 
--- go back to start and update the score
+     -- if we reach the left or right edge of the screen, 
+     -- go back to start and update the score
      if ball.x < 0 then
+          sounds['score']:play()
           servingPlayer = 1
           player2Score = player2Score + 1
           ball:reset()
@@ -94,6 +112,7 @@ function love.update(dt)
      end
 
      if ball.x > VIRTUAL_WIDTH then
+          sounds['score']:play()
           servingPlayer = 2
           player1Score = player1Score + 1
           ball:reset()
